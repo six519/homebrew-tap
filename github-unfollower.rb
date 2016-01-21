86cd1e0744ceafc51c6f845f629f0b4e94b413a7
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2016-01-21 16:26:08 +0800

    update

--- github-unfollower.rb
+++ github-unfollower.rb
@@ -9,9 +9,24 @@ class GithubUnfollower < Formula
   version "1.0.0"
   sha256 "4d1f30852c6a1d69c75c9e4e3680b635abe393422a2786570539668a4b684279"
 
+  resource "PyGithub" do
+    url "https://pypi.python.org/packages/source/P/PyGithub/PyGithub-1.26.0.tar.gz"
+    sha256 "59d0ceafd1c4fb73117aa35f515988b691a386046714f278521ba01d260ddefb"
+  end
+
   def install
-    system "pip install PyGithub" 
-    bin.install "github_unfollower"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[PyGithub].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
+    bin.install Dir["#{libexec}/bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
 end
